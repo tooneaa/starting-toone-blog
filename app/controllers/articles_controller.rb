77868cba88e92171ea3c24e0ae5bf3062798e782
toone <tooneaa@hotmail.com>
2019-02-27 13:44:52 +0000

    create action updated, show action added, error messages added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
   
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:articles].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:alert] = "Article was saved successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
+  
+  def show
+    @article = Article.find(params[:id])
+    
+  end
+
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
   
 end
