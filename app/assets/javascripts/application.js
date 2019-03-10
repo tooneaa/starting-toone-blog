54a697ea29977bdedd3ec74c435b16b47ae2563f
toone <tooneaa@hotmail.com>
2019-03-10 08:13:42 +0000

    added css for homepage with footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap-sprockets
+
 //= require rails-ujs
+//= require jquery3
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
