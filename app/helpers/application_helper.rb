3634ae212692754e1874f9b2556beabec09a4996
toone <tooneaa@hotmail.com>
2019-03-24 11:56:44 +0000

    added all user listing, added user detail in ArticleShow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def gravatar_for (user, size)
+  def gravatar_for (user, size = 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class:"img-circle" )
