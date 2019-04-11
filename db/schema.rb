1179989fa3d8cec40ee8b3e7ba206229fb1f15e0
toone <tooneaa@hotmail.com>
2019-04-11 15:23:49 +0000

    added admin function to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_105123) do
+ActiveRecord::Schema.define(version: 2019_04_11_144459) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
